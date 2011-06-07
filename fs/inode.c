0ef9559de89e8eeb8113a8cdd10ddfb83041654a
Matt B <jackdachef@gmail.com>
2011-06-07 06:22:00 +0200

    fs: update atime from future

--- fs/inode.c
+++ fs/inode.c
@@ -1381,7 +1381,13 @@ static int relatime_need_update(struct vfsmount *mnt, struct inode *inode,
 		return 1;
 
 	/*
-	 * Is the previous atime value older than a day? If yes,
+	 * Is the previous atime value in future? If yes,
+	 * update atime:
+	 */
+	if ((long)(now.tv_sec - inode->i_atime.tv_sec) < 0)
+		return 1;
+	/*
+	 * Is the previous atime value old than a day? If yes,
 	 * update atime:
 	 */
 	if ((long)(now.tv_sec - inode->i_atime.tv_sec) >= 24*60*60)
