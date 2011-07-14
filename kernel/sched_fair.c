d9c70650b0e768ed60aed2bac9974cded2e93a99
LorD ClockaN <davor@losinj.com>
2011-07-14 14:59:03 +0200

    TWEAK: Tweaked BFS a little

--- kernel/sched_fair.c
+++ kernel/sched_fair.c
@@ -84,8 +84,8 @@ unsigned int __read_mostly sysctl_sched_compat_yield;
  * and reduces their over-scheduling. Synchronous workloads will still
  * have immediate wakeup/sleep latencies.
  */
-unsigned int sysctl_sched_wakeup_granularity = 1000000UL;
-unsigned int normalized_sysctl_sched_wakeup_granularity = 1000000UL;
+unsigned int sysctl_sched_wakeup_granularity = 25000UL;
+unsigned int normalized_sysctl_sched_wakeup_granularity = 25000UL;
 
 const_debug unsigned int sysctl_sched_migration_cost = 500000UL;
 
