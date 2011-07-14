baecf4e9f62735aedb6837994114fc5af1222d4b
LorD ClockaN <davor@losinj.com>
2011-07-14 13:36:22 +0200

    TWEAK: Tweaked brazilianwax to hopefully scale better

--- drivers/cpufreq/cpufreq_brazilianwax.c
+++ drivers/cpufreq/cpufreq_brazilianwax.c
@@ -141,18 +141,18 @@ static unsigned int ramp_down_step;
 /*
  * CPU freq will be increased if measured load > max_cpu_load;
  */
-#define DEFAULT_MAX_CPU_LOAD 45
+#define DEFAULT_MAX_CPU_LOAD 65
 static unsigned long max_cpu_load;
 
-#define DEFAULT_X_CPU_LOAD 70
+#define DEFAULT_X_CPU_LOAD 80
 static unsigned long x_cpu_load;
 
 /*
  * CPU freq will be decreased if measured load < min_cpu_load;
  */
-#define DEFAULT_MIN_CPU_LOAD 25
+#define DEFAULT_MIN_CPU_LOAD 35
 static unsigned long min_cpu_load;
-#define RAPID_MIN_CPU_LOAD 5
+#define RAPID_MIN_CPU_LOAD 10
 static unsigned long rapid_min_cpu_load;
 
 
