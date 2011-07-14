307a762ec7ed4a6aa952ee9d01fa156713c975f2
LorD ClockaN <davor@losinj.com>
2011-07-14 13:35:27 +0200

    ADD: Upped the max voltage in VDD to 1.55V

--- arch/arm/mach-msm/acpuclock-7x30.c
+++ arch/arm/mach-msm/acpuclock-7x30.c
@@ -56,7 +56,7 @@
 #define PLL2_L_VAL_ADDR  (MSM_CLK_CTL_BASE + 0x33c)
 
 #define ACE_ACPU_MIN_UV_MV 700U
-#define ACE_ACPU_MAX_UV_MV 1500U
+#define ACE_ACPU_MAX_UV_MV 1550U
 
 struct clock_state {
 	struct clkctl_acpu_speed	*current_speed;
