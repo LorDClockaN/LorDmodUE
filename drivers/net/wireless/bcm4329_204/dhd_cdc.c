7cbec74c366f877e58152c3c851b5ee64f14cf41
LorD ClockaN <davor@losinj.com>
2011-07-24 19:38:47 +0200

    Edit: WIFI PM=FAST in standby

--- drivers/net/wireless/bcm4329_204/dhd_cdc.c
+++ drivers/net/wireless/bcm4329_204/dhd_cdc.c
@@ -1347,10 +1347,7 @@ int dhdhtc_update_wifi_power_mode(int is_screen_off)
 		pm_type = PM_OFF;
 		dhdcdc_set_ioctl(dhd, 0, WLC_SET_PM, &pm_type, sizeof(pm_type));
 	} else {
-		if (is_screen_off && !dhdcdc_wifiLock)
-			pm_type = PM_MAX;
-		else
-			pm_type = PM_FAST;
+		pm_type = PM_FAST;
 		myprintf("update pm: %s, wifiLock: %d\n", pm_type==1?"PM_MAX":"PM_FAST", dhdcdc_wifiLock);
 		dhdcdc_set_ioctl(dhd, 0, WLC_SET_PM, &pm_type, sizeof(pm_type));
 	}
