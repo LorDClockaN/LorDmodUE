3aecd1d8146d0742f3f54289082de4b213b2f99c
LorD ClockaN <davor@losinj.com>
2011-07-29 18:48:46 +0200

    EDIT: Build.sh edited with make flags for gcc 4.5.2

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 # Script taken from Imoseyon and modified by LorD CLockaN #
 
 CPU_JOB_NUM=4
-TOOLCHAIN=/home/lord/android/system/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/
+TOOLCHAIN=/home/lord/android/system/prebuilt/linux-x86/toolchain/arm-2011/bin/
 TOOLCHAIN_PREFIX=arm-eabi-
 
 
-make -j$CPU_JOB_NUM ARCH=arm CROSS_COMPILE=$TOOLCHAIN/$TOOLCHAIN_PREFIX;
+make -j4 ARCH=arm CROSS_COMPILE=$CCOMPILER EXTRA_AFLAGS='-mfpu=neon -ftree-vectorize -mfloat-abi=softfp';
+
 
 
 cp arch/arm/boot/zImage ../finished/;
