92b8304ee9da918ea511a1aa9a8f7a14de5b5d70
Yamashita TAKAO <tjy1965@gmail.com>
2011-08-26 11:31:16 +0900

    add Aufs2

--- fs/namespace.c
+++ fs/namespace.c
@@ -1279,6 +1279,7 @@ int iterate_mounts(int (*f)(struct vfsmount *, void *), void *arg,
 	}
 	return 0;
 }
+EXPORT_SYMBOL(iterate_mounts);
 
 static void cleanup_group_ids(struct vfsmount *mnt, struct vfsmount *end)
 {
