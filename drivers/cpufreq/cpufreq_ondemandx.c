0984b7493dc16ac33f4d479babfe44be16d5e31d
LorD ClockaN <davor@losinj.com>
2011-09-02 16:00:15 +0200

    FIX: FIxed Ondemand governor by making it OndemandX with suspend freq of 998MHz

--- drivers/cpufreq/cpufreq_ondemandx.c
+++ drivers/cpufreq/cpufreq_ondemandx.c
@@ -67,7 +67,7 @@ static int cpufreq_governor_dbs(struct cpufreq_policy *policy,
 static
 #endif
 struct cpufreq_governor cpufreq_gov_ondemandx = {
-       .name                   = "ondemandX",
+       .name                   = "ondemandx",
        .governor               = cpufreq_governor_dbs,
        .max_transition_latency = TRANSITION_LATENCY_LIMIT,
        .owner                  = THIS_MODULE,
