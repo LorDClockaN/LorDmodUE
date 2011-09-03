7204a481435614b1d27c8fd0670668da421f7514
existz <robbeane@gmail.com>
2011-09-03 15:17:04 -0400

    cpufreq: add updated smartassv2 governor [erasmux]

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -378,6 +378,9 @@ extern struct cpufreq_governor cpufreq_gov_minmax;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS)
 extern struct cpufreq_governor cpufreq_gov_smartass;
 #define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_smartass)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS2)
+extern struct cpufreq_governor cpufreq_gov_smartass2;
+#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_smartass2)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BRAZILIANWAX)
 extern struct cpufreq_governor cpufreq_gov_brazilianwax;
 #define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_brazilianwax)
