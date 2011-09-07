af78b90268430c772a6743418b34f7f3cf88a179
Yamashita TAKAO <tjy1965@gmail.com>
2011-09-08 04:43:45 +0900

    update:lordmodaufs_defconfig

--- kern_build.sh
+++ kern_build.sh
@@ -2,17 +2,19 @@
 
 # chrooted gentoo on DesireHD.
 
-ARCH=arm
-CROSS_COMPILE=
+ARCH=${ARCH:=arm}
+CROSS_COMPILE=${CROSS_COMPILE:=}
 
 cpuinfo=$(grep -i processor /proc/cpuinfo | wc -l)
-git_ver=$(git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/*\s//g')
-obj_dir="../build/${git_ver}/obj"
-finished="../build/${git_ver}/finished"
+git_repo=$(git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/*\s//g')
+obj_dir="$(readlink -f ..)/build/${git_repo}/obj"
+finished="$(readlink -f ..)/build/${git_repo}/finished"
+anykernel_dir="$(readlink -f ..)/build/AnyKernel"
 def_config=lordmodaufs_defconfig
 
 die() {
-    echo -e "\033[1;30m>\033[0;31m>\033[1;31m> ERROR:\033[0m ${@}" && exit 1
+    echo -e "\033[1;30m>\033[0;31m>\033[1;31m> ERROR:\033[0m ${@}"
+    exit 1
 }
 
 einfo() {
@@ -38,43 +40,66 @@ config_kernel ()
 compile_kernel () {
 	einfo "Compile kernel"
 	make -j${cpuinfo} ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE \
+		KERNEL_DIR=$KERNEL_DIR \
 		EXTRA_AFLAGS='-mfpu=vfpv3 -ftree-vectorize -floop-interchange -floop-strip-mine -floop-block' \
-		O=${obj_dir} zImage 2>&1 | tee ${obj_dir}/kern.log || die "Compile kernel error"
+		O=${obj_dir} zImage 2>&1 || die "Compile kernel error"
 	einfo "Compile modules"
 	make -j${cpuinfo} ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE \
+		KERNEL_DIR=$KERNEL_DIR \
 		EXTRA_AFLAGS='-mfpu=vfpv3 -ftree-vectorizeine -floop-interchange -floop-strip-mine -floop-block' \
-		O=${obj_dir} modules 2>&1 | tee ${obj_dir}/kern.log || die "Compile module error"
+		O=${obj_dir} modules 2>&1 || die "Compile module error"
+}
+
+setup_anykernel () {
+	test -d $anykernel_dir || 
+		git clone git://github.com/ac1965/AnyKernel.git $anykernel_dir 2>&1 > /dev/null
+	(
+		cd $anykernel_dir
+		git pull 2>&1 >/dev/null
+		git archive --format zip --output ../u.zip master 2>&1 > /dev/null
+	)
+	test -d $finished && rm -fr $finished
+	unzip ../build/u.zip -d $finished 2>&1 > /dev/null
+	rm -f ../build/u.zip
 }
 
 install_kernel () {
 	einfo  "Install modules"
 	rm -fr ../build/install
 	make -j${cpuinfo} ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE \
+		KERNEL_DIR=$KERNEL_DIR \
 		EXTRA_AFLAGS='-mfpu=vfpv3 -ftree-vectorize -floop-interchange -floop-strip-mine -floop-block' \
 		O=${obj_dir} modules_install INSTALL_MOD_PATH=../install 2>&1 >/dev/null
 
-	einfo "Install packages"
-	test -d ${finished}/system/lib/modules || install -d ${finished}/system/lib/modules
-	test -d ${finished}/kernel || install -d ${finished}/kernel
-	cp ${obj_dir}/arch/arm/boot/zImage ${finished}/kernel/
-	for f in $(find ../build/${git_ver}/install -name "*.ko")
+	einfo "Setup Anykernel"
+	setup_anykernel
+	dt=$(date +%Y%m%d%H%M)
+	zipedf="update-${dt}.zip"
+
+	einfo "Install packages [$zipedf]"
+	find ${obj_dir}/arch/${ARCH}/boot -name zImage -exec install -D {} ${finished}/kernel/zImage \;
+	for f in $(find ../build/${git_repo}/install -name "*.ko")
 	do
-		echo " $(basename $f)"
+		echo " - $(basename $f)"
 		cp $f ${finished}/system/lib/modules/
 	done
+	(
+		cd ${finished}
+		zip -r ../${zipedf} .
+	)
 }
 
 test -d ${obj_dir} || install -d ${obj_dir}
 test -f ${obj_dir}/.config || config_kernel
 
 if [ -d .git ]; then
-	einfo "Pulling git repository: $git_ver"
-	git pull 2>&1 >/dev/null || die "Does not pulling from repository"
+	KERNEL_DIR=$(readlink -f $(dirname .))
+	einfo "Git Repository: $git_repo"
 
 	einfo "Clean-up old modules"
 	test -d ${finished}/system/lib/modules && rm -f ${finished}/system/lib/modules/*
 	test -d ${finished}/kernel/zImage && rm -f ${finished}/kernel/zImage
-	find ${obj_build} -name "*.ko" | xargs rm -f
+	find ${obj_dir} -name "*.ko" | xargs rm -f
 
 	compile_kernel
 	install_kernel
