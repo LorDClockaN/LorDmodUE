c627a13b627f5e6aef7a92a05d1d00cbdc3ff065
LorD ClockaN <davor@losinj.com>
2011-10-13 16:02:14 +0200

    add default vm_swappiness value of 90

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -125,7 +125,7 @@ struct scan_control {
 /*
  * From 0 .. 100.  Higher means more swappy.
  */
-int vm_swappiness;
+int vm_swappiness = 90;
 long vm_total_pages;	/* The total number of pages which the VM controls */
 
 static LIST_HEAD(shrinker_list);
