e07b715888273c11ea0e337d9c6bcc20a4c83c47
LorD ClockaN <davor@losinj.com>
2011-10-13 17:43:38 +0200

    REVERT swappiness because francofrancisco told me that I'm stupid :P

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -125,7 +125,7 @@ struct scan_control {
 /*
  * From 0 .. 100.  Higher means more swappy.
  */
-int vm_swappiness = 90;
+int vm_swappiness;
 long vm_total_pages;	/* The total number of pages which the VM controls */
 
 static LIST_HEAD(shrinker_list);
