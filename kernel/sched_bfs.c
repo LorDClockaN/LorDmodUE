a27cb3fb397e28c8fa8c7c9185e92c5e16dad523
LorD ClockaN <davor@losinj.com>
2011-10-30 16:55:54 +0100

    TWEAK: set rr_interval to 2 for BFS kernel

--- kernel/sched_bfs.c
+++ kernel/sched_bfs.c
@@ -127,7 +127,7 @@
  * Value is in ms and set to a minimum of 6ms. Scales with number of cpus.
  * Tunable via /proc interface.
  */
-int rr_interval __read_mostly = 6;
+int rr_interval __read_mostly = 2;
 
 /*
  * sched_iso_cpu - sysctl which determines the cpu percentage SCHED_ISO tasks
