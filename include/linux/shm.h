09c5b13654ca42f4a1d5356a32582ec4a2cc7aed
LorD ClockaN <davor@losinj.com>
2011-11-02 16:26:55 +0100

    added option to disable kernelizer tweaks in config

--- include/linux/shm.h
+++ include/linux/shm.h
@@ -14,7 +14,11 @@
  * be increased by sysctl
  */
 
+#ifdef CONFIG_KERNELIZER
 #define SHMMAX 0x268435456		 /* max shared seg size (bytes) */
+#else
+#define SHMMAX 0x2000000		 /* max shared seg size (bytes) */
+#endif
 #define SHMMIN 1			 /* min shared seg size (bytes) */
 #define SHMMNI 4096			 /* max num of segs system wide */
 #ifdef __KERNEL__
--- kernel/sched.c
+++ kernel/sched.c
@@ -709,9 +709,15 @@ enum {
 #define SCHED_FEAT(name, enabled)	\
 	(1UL << __SCHED_FEAT_##name) * enabled |
 
+#ifdef CONFIG_KERNELIZER
 const_debug unsigned int sysctl_sched_features =
 #include "sched_features.h"
 	24189;
+#else
+const_debug unsigned int sysctl_sched_features =
+#include "sched_features.h"
+	0;
+#endif
 
 #undef SCHED_FEAT
 
