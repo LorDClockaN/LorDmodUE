f0e89e4d007394c8540cee20b31312ec63a70815
existz <robbeane@gmail.com>
2011-11-02 13:05:50 -0400

    msm: kgsl: Use timer instead of loop count to wait gpu to idle
    Previously kgsl_yamato_idle() used a loop count to check gpu hang.
    This count is not big enough for the test app so a gpu hang was
    reported too soon. A drawback of using loop constant is inconsistent
    waiting time for different cpu speeds. To fix this, read cpu time to
    check timeout. Also leave more time to wait for gpu to finish its work.
    
    cherry-picked from:
    https://www.codeaurora.org/gitweb/quic/la/?p=kernel/msm.git;a=commitdiff;h=d86b52649abab4adddd672537ea0c9e9e9e4ee73

--- drivers/video/msm/gpu/kgsl_adreno205/kgsl_yamato.c
+++ drivers/video/msm/gpu/kgsl_adreno205/kgsl_yamato.c
@@ -830,39 +830,38 @@ static int kgsl_yamato_getproperty(struct kgsl_device *device,
 	return status;
 }
 
+/* max msecs to wait for gpu to finish its operation(s) */
+#define MAX_WAITGPU_SECS (HZ + HZ/2)
+
 /* Caller must hold the driver mutex. */
 int kgsl_yamato_idle(struct kgsl_device *device, unsigned int timeout)
 {
-	int status = -EINVAL;
 	struct kgsl_ringbuffer *rb = &device->ringbuffer;
 	struct kgsl_mmu_debug mmu_dbg;
 	unsigned int rbbm_status;
-	int idle_count = 0;
-#define IDLE_COUNT_MAX 1500000
 
-	KGSL_DRV_VDBG("enter (device=%p, timeout=%d)\n", device, timeout);
+	unsigned long wait_time = jiffies + MAX_WAITGPU_SECS;
 
-	(void)timeout;
+	KGSL_DRV_VDBG("enter (device=%p)\n", device);
 
 	/* first, wait until the CP has consumed all the commands in
 	 * the ring buffer
 	 */
 	if (rb->flags & KGSL_FLAGS_STARTED) {
 		do {
-			idle_count++;
 			GSL_RB_GET_READPTR(rb, &rb->rptr);
 
-		} while (rb->rptr != rb->wptr && idle_count < IDLE_COUNT_MAX);
-		if (idle_count == IDLE_COUNT_MAX)
+		if (time_after(jiffies, wait_time))
 			goto err;
+		} while (rb->rptr != rb->wptr);
 	}
 	/* now, wait for the GPU to finish its operations */
-	for (idle_count = 0; idle_count < IDLE_COUNT_MAX; idle_count++) {
+	wait_time = jiffies + MAX_WAITGPU_SECS;
+	while (time_before(jiffies, wait_time)) {
 		kgsl_yamato_regread(device, REG_RBBM_STATUS, &rbbm_status);
-
 		if (rbbm_status == 0x110) {
-			status = 0;
-			goto done;
+			KGSL_DRV_VDBG("return %d\n", 0);
+			return 0;
 		}
 	}
 
@@ -873,10 +872,7 @@ err:
 	kgsl_mmu_debug(&device->mmu, &mmu_dbg);
 	BUG();
 
-done:
-	KGSL_DRV_VDBG("return %d\n", status);
-
-	return status;
+	return -ETIMEDOUT;
 }
 
 static unsigned int kgsl_yamato_isidle(struct kgsl_device *device)
@@ -953,7 +949,7 @@ static int kgsl_yamato_suspend(struct kgsl_device *device)
 	int status;
 
 	/* Wait for the device to become idle */
-	status = kgsl_yamato_idle(device, IDLE_COUNT_MAX);
+	status = kgsl_yamato_idle(device, KGSL_TIMEOUT_DEFAULT);
 
 	if (status == KGSL_SUCCESS) {
 		/* Put the device to sleep. */
