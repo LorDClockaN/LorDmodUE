41728a10e22a2981479d19210199c153462c16e9
LorD ClockaN <davor@losinj.com>
2011-11-04 16:12:22 +0100

    sched: Do not block when waiting to free old root domain
    Instead of calling synchronize_sched(), directly invoke
    call_rcu_sched() which will not block the cpu hotplug
    operation but will call free_rootdomain when the rcu
    process is complete. This is safe because the root
    domains are allocated dynamically so any new readers
    will have pointers to the new data and the new data
    does not overwrite the old data.
    
    This is a partial backport from 3.0.0 kernel.
    I did not backport the dynamic allocation of the sched
    domain and sched group data structures.  This would
    require inclusion of 10 other patches and would not be
    stable.
    
    dce840a08702bd13a9a186e07e63d1ef82256b5e
    Author: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Date:   Thu Apr 7 14:09:50 2011 +0200
    
        sched: Dynamically allocate sched_domain/sched_group data-structures
    
        Instead of relying on static allocations for the sched_domain and
        sched_group trees, dynamically allocate and RCU free them.
    
        Allocating this dynamically also allows for some build_sched_groups()
        simplification since we can now (like with other simplifications) rely
        on the sched_domain tree instead of hard-coded knowledge.
    
        One tricky to note is that detach_destroy_domains() needs to hold
        rcu_read_lock() over the entire tear-down, per-cpu is not sufficient
        since that can lead to partial sched_group existance (could possibly
        be solved by doing the tear-down backwards but this is much more
        robust).
    
        A concequence of the above is that we can no longer print the
        sched_domain debug stuff from cpu_attach_domain() since that might now
        run with preemption disabled (due to classic RCU etc.) and
        sched_domain_debug() does some GFP_KERNEL allocations.
    
        Another thing to note is that we now fully rely on normal RCU and not
        RCU-sched, this is because with the new and exiting RCU flavours we
        grew over the years BH doesn't necessarily hold off RCU-sched grace
        periods (-rt is known to break this). This would in fact already cause
        us grief since we do sched_domain/sched_group iterations from softirq
        context.
    
        This patch is somewhat larger than I would like it to be, but I didn't
        find any means of shrinking/splitting this.
    
    Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Nick Piggin <npiggin@kernel.dk>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Andrew Morton <akpm@linux-foundation.org>
        Link: http://lkml.kernel.org/r/20110407122942.245307941@chello.nl
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    
    Signed-off-by: Maya Spivak <mspivak@codeaurora.org>
    (cherry picked from commit 09d39b8ade298ffed33597926a1b30f4609400a6)
    
    Change-Id: I7305af949900919f06cf3444daf1183370aa07b7
    Signed-off-by: Swetha Basineni <sbasin@codeaurora.org>

--- kernel/sched.c
+++ kernel/sched.c
@@ -76,6 +76,7 @@
 #include <linux/ftrace.h>
 #include <linux/slab.h>
 #include <linux/cpuacct.h>
+#include <linux/kernel.h>
 #include "sched_autogroup.h"
 
 #include <asm/tlb.h>
@@ -425,6 +426,7 @@ struct rt_rq {
  */
 struct root_domain {
 	atomic_t refcount;
+	struct rcu_head rcu;
 	cpumask_var_t span;
 	cpumask_var_t online;
 
@@ -6363,9 +6365,9 @@ sd_parent_degenerate(struct sched_domain *sd, struct sched_domain *parent)
 	return 1;
 }
 
-static void free_rootdomain(struct root_domain *rd)
+static void free_rootdomain(struct rcu_head *rcu)
 {
-	synchronize_sched();
+	struct root_domain *rd = container_of(rcu, struct root_domain, rcu);
 
 	cpupri_cleanup(&rd->cpupri);
 
@@ -6409,7 +6411,7 @@ static void rq_attach_root(struct rq *rq, struct root_domain *rd)
 	raw_spin_unlock_irqrestore(&rq->lock, flags);
 
 	if (old_rd)
-		free_rootdomain(old_rd);
+		call_rcu_sched(&old_rd->rcu, free_rootdomain);
 }
 
 static int init_rootdomain(struct root_domain *rd, bool bootmem)
@@ -7054,7 +7056,7 @@ static void __free_domain_allocs(struct s_data *d, enum s_alloc what,
 		free_sched_groups(cpu_map, d->tmpmask); /* fall through */
 		d->sched_group_nodes = NULL;
 	case sa_rootdomain:
-		free_rootdomain(d->rd); /* fall through */
+		free_rootdomain(&(d->rd->rcu)); /* fall through */
 	case sa_tmpmask:
 		free_cpumask_var(d->tmpmask); /* fall through */
 	case sa_send_covered:
