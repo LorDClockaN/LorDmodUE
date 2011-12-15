9aa8819052909c578143825554b51ddbb67711eb
LorD ClockaN <davor@losinj.com>
2011-12-15 14:34:18 +0100

    ARM: Move leds idle start/stop calls to idle notifiers
    Change-Id: I5d8e4e85b17bbab7992ecb477f0bdb5e4138b166
    Acked-by: Nicolas Pitre <nicolas.pitre@linaro.org>
    Signed-off-by: Todd Poynor <toddpoynor@google.com>

--- arch/arm/kernel/process.c
+++ arch/arm/kernel/process.c
@@ -31,6 +31,7 @@
 
 #include <asm/cacheflush.h>
 #include <asm/leds.h>
+#include <asm/cacheflush.h>
 #include <asm/processor.h>
 #include <asm/system.h>
 #include <asm/thread_notify.h>
@@ -156,7 +157,6 @@ void cpu_idle(void)
 	/* endless idle loop with no priority at all */
 	while (1) {
 		tick_nohz_stop_sched_tick(1);
-		leds_event(led_idle_start);
 		while (!need_resched()) {
 		idle_notifier_call_chain(IDLE_START);
 #ifdef CONFIG_HOTPLUG_CPU
@@ -181,7 +181,6 @@ void cpu_idle(void)
 				local_irq_enable();
 			}
 		}
-		leds_event(led_idle_end);
 		idle_notifier_call_chain(IDLE_END);
 		tick_nohz_restart_sched_tick();
 		preempt_enable_no_resched();
