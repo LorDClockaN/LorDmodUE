6f44e4a55d24da61eded3c540470d435d43b1458
LorD ClockaN <davor@losinj.com>
2011-12-23 21:14:47 +0100

    REVERT: for compatibility 	msm: Increase the DMA consistent memory zone to 14MB

--- arch/arm/mach-msm/include/mach/memory.h
+++ arch/arm/mach-msm/include/mach/memory.h
@@ -78,7 +78,7 @@
 
 #define HAS_ARCH_IO_REMAP_PFN_RANGE
 
-#define CONSISTENT_DMA_SIZE (SZ_1M * 14)
+#define CONSISTENT_DMA_SIZE (4*SZ_1M)
 
 #ifndef __ASSEMBLY__
 void *alloc_bootmem_aligned(unsigned long size, unsigned long alignment);
