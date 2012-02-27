36152e2463694477750d59778558219c838501d2
Twisted <twistedumbrella@gmail.com>
2012-02-27 11:55:55 -0500

    UPDATE: Multiple kernel patches [Code Aurora]

--- arch/arm/mach-msm/sirc.c
+++ arch/arm/mach-msm/sirc.c
@@ -3,14 +3,15 @@
  * Copyright (c) 2009-2010 Code Aurora Forum, All rights reserved.
  * Copyright (C) 2009 Google, Inc.
  *
- * This software is licensed under the terms of the GNU General Public
- * License version 2, as published by the Free Software Foundation, and
- * may be copied, distributed, and modified under those terms.
- *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
  *
  */
 
