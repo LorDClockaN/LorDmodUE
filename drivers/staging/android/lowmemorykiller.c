422fd47eae8b7758a60b09b3b22159638fe2a318
Jie W <cn.fyodor@gmail.com>
2012-04-15 17:49:10 +0800

    android: lowmemorykiller: new version from ics_chocolate

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -35,8 +35,8 @@
 #include <linux/oom.h>
 #include <linux/sched.h>
 #include <linux/notifier.h>
-
-#define DEBUG_LEVEL_DEATHPENDING 6
+#include <linux/memory.h>
+#include <linux/memory_hotplug.h>
 
 static uint32_t lowmem_debug_level = 2;
 static int lowmem_adj[6] = {
@@ -54,26 +54,14 @@ static size_t lowmem_minfree[6] = {
 };
 static int lowmem_minfree_size = 4;
 
-static size_t lowmem_minfile[6] = {
-	1536,
-	2048,
-	4096,
-	5120,
-	5632,
-	6144
-};
-static int lowmem_minfile_size = 6;
-
+static unsigned int offlining;
 static struct task_struct *lowmem_deathpending;
 static unsigned long lowmem_deathpending_timeout;
-static uint32_t lowmem_check_filepages = 0;
 
 #define lowmem_print(level, x...)			\
 	do {						\
-		if (lowmem_debug_level >= (level)) {	\
-			printk("lowmem: ");		\
+		if (lowmem_debug_level >= (level))	\
 			printk(x);			\
-		}					\
 	} while (0)
 
 static int
@@ -88,50 +76,35 @@ task_notify_func(struct notifier_block *self, unsigned long val, void *data)
 {
 	struct task_struct *task = data;
 
-	if (task == lowmem_deathpending) {
+	if (task == lowmem_deathpending)
 		lowmem_deathpending = NULL;
-		lowmem_print(2, "deathpending end %d (%s)\n",
-			task->pid, task->comm);
-	}
 
 	return NOTIFY_OK;
 }
 
-static void dump_deathpending(struct task_struct *t_deathpending)
+#ifdef CONFIG_MEMORY_HOTPLUG
+static int lmk_hotplug_callback(struct notifier_block *self,
+				unsigned long cmd, void *data)
 {
-	struct task_struct *p;
-
-	if (lowmem_debug_level < DEBUG_LEVEL_DEATHPENDING)
-		return;
-
-	BUG_ON(!t_deathpending);
-	lowmem_print(DEBUG_LEVEL_DEATHPENDING, "deathpending %d (%s)\n",
-		t_deathpending->pid, t_deathpending->comm);
-
-	read_lock(&tasklist_lock);
-	for_each_process(p) {
-		struct mm_struct *mm;
-		struct signal_struct *sig;
-		int oom_adj;
-		int tasksize;
-
-		task_lock(p);
-		mm = p->mm;
-		sig = p->signal;
-		if (!mm || !sig) {
-			task_unlock(p);
-			continue;
-		}
-		oom_adj = sig->oom_adj;
-		tasksize = get_mm_rss(mm);
-		task_unlock(p);
-		lowmem_print(DEBUG_LEVEL_DEATHPENDING,
-			"  %d (%s), adj %d, size %d\n",
-			p->pid, p->comm,
-			oom_adj, tasksize);
+	switch (cmd) {
+	/* Don't care LMK cases */
+	case MEM_ONLINE:
+	case MEM_OFFLINE:
+	case MEM_CANCEL_ONLINE:
+	case MEM_CANCEL_OFFLINE:
+	case MEM_GOING_ONLINE:
+		offlining = 0;
+		lowmem_print(4, "lmk in normal mode\n");
+		break;
+	/* LMK should account for movable zone */
+	case MEM_GOING_OFFLINE:
+		offlining = 1;
+		lowmem_print(4, "lmk in hotplug mode\n");
+		break;
 	}
-	read_unlock(&tasklist_lock);
+	return NOTIFY_DONE;
 }
+#endif
 
 static int lowmem_shrink(struct shrinker *s, int nr_to_scan, gfp_t gfp_mask)
 {
@@ -147,9 +120,20 @@ static int lowmem_shrink(struct shrinker *s, int nr_to_scan, gfp_t gfp_mask)
 	int other_free = global_page_state(NR_FREE_PAGES);
 	int other_file = global_page_state(NR_FILE_PAGES) -
 						global_page_state(NR_SHMEM);
-	int lru_file = global_page_state(NR_ACTIVE_FILE) +
-			global_page_state(NR_INACTIVE_FILE);
+	struct zone *zone;
 
+	if (offlining) {
+		/* Discount all free space in the section being offlined */
+		for_each_zone(zone) {
+			 if (zone_idx(zone) == ZONE_MOVABLE) {
+				other_free -= zone_page_state(zone,
+						NR_FREE_PAGES);
+				lowmem_print(4, "lowmem_shrink discounted "
+					"%lu pages in movable zone\n",
+					zone_page_state(zone, NR_FREE_PAGES));
+			}
+		}
+	}
 	/*
 	 * If we already have a death outstanding, then
 	 * bail out right away; indicating to vmscan
@@ -158,24 +142,18 @@ static int lowmem_shrink(struct shrinker *s, int nr_to_scan, gfp_t gfp_mask)
 	 *
 	 */
 	if (lowmem_deathpending &&
-	    time_before_eq(jiffies, lowmem_deathpending_timeout)) {
-		dump_deathpending(lowmem_deathpending);
+	    time_before_eq(jiffies, lowmem_deathpending_timeout))
 		return 0;
-	}
 
 	if (lowmem_adj_size < array_size)
 		array_size = lowmem_adj_size;
 	if (lowmem_minfree_size < array_size)
 		array_size = lowmem_minfree_size;
 	for (i = 0; i < array_size; i++) {
-		if (other_free < lowmem_minfree[i]) {
-			if (other_file < lowmem_minfree[i] ||
-				(lowmem_check_filepages &&
-				(lru_file < lowmem_minfile[i]))) {
-
-				min_adj = lowmem_adj[i];
-				break;
-			}
+		if (other_free < lowmem_minfree[i] &&
+		    other_file < lowmem_minfree[i]) {
+			min_adj = lowmem_adj[i];
+			break;
 		}
 	}
 	if (nr_to_scan > 0)
@@ -252,6 +230,9 @@ static int __init lowmem_init(void)
 {
 	task_free_register(&task_nb);
 	register_shrinker(&lowmem_shrinker);
+#ifdef CONFIG_MEMORY_HOTPLUG
+	hotplug_memory_notifier(lmk_hotplug_callback, 0);
+#endif
 	return 0;
 }
 
@@ -267,11 +248,6 @@ module_param_array_named(adj, lowmem_adj, int, &lowmem_adj_size,
 module_param_array_named(minfree, lowmem_minfree, uint, &lowmem_minfree_size,
 			 S_IRUGO | S_IWUSR);
 module_param_named(debug_level, lowmem_debug_level, uint, S_IRUGO | S_IWUSR);
-
-module_param_named(check_filepages , lowmem_check_filepages, uint,
-		   S_IRUGO | S_IWUSR);
-module_param_array_named(minfile, lowmem_minfile, uint, &lowmem_minfile_size,
-			 S_IRUGO | S_IWUSR);
 
 module_init(lowmem_init);
 module_exit(lowmem_exit);
